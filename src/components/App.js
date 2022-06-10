0375406c0418f7c9beea145409f712899c272e16
AIG ROYAL <alpineinfraprojects@gmail.com>
2022-06-10 17:59:34 +0530

    bar chart and pie chart implementation

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import UseEffectApi from './UseEffectApi';
 import PostForm from './PostForm';
+import PieChart from './PieChart';
+import 'bootstrap';
+import BarChart from './BarChart';
 
 function App() {
   return (
@@ -19,6 +22,16 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path='/bookingForm' element={<PostForm />} />
+        </Routes>
+      </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/pieChart' element={<PieChart />} />
+        </Routes>
+      </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/barChart' element={<BarChart />} />
         </Routes>
       </BrowserRouter>
     </>
