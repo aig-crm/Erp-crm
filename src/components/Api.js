9f0684114fca2bc2701551dab04f6e2f3b1d06a5
AIG ROYAL <alpineinfraprojects@gmail.com>
2022-09-11 12:18:25 +0530

    11/09

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default axios.create({
-  baseURL: "https://93a3-103-163-109-164.in.ngrok.io/api",
+  baseURL: "https://2d67-103-163-108-204.in.ngrok.io/api",
   headers: {
   },
 });
